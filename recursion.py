0fd5e7bb499a3164656166359463fe4c1ad97270
Siddh <xpress.siddharth@gmail.com>
2022-11-14 21:51:08 +0530

    Recursion 2

--- recursion.py
+++ recursion.py
@@ -1,5 +1,13 @@
 
 def fact(n):
+    """_summary_    : Finds Factorial of a number
+
+    Args:
+        n (_type_): _description_   : Number
+
+    Returns:
+        _type_: _description_  : Factorial of the number
+    """
     if n == 0:
         return 1
     return n * fact(n-1)
@@ -8,8 +16,8 @@ def fact(n):
 def sum_n(n):
     if n == 0:
         return 0
-    smallOutput = sum_n(n-1)
-    output = smallOutput + n
+    small_output = sum_n(n-1)
+    output = small_output + n
     return output
 
 
